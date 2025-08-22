806aff1a0ad6944af202cc554ad4b7a9f6826df4
keyur bhadani <bhadani@hectronic.in>
2025-08-22 18:20:04 +0530

    feat:support multiple delimiters of any length

--- src/main/java/com/incubyte/Calculator.java
+++ src/main/java/com/incubyte/Calculator.java
@@ -1,5 +1,12 @@
 package com.incubyte;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
 public class Calculator {
     private int callCount = 0;
 
@@ -9,25 +16,60 @@ public class Calculator {
         if (numbers == null || numbers.isEmpty()) return 0;
 
         String delimitersRegex = ",|\n"; // default
-        String payload = numbers;
+
 
         if (numbers.startsWith("//")) {
 
-            int nl = numbers.indexOf('\n');
-            String header = numbers.substring(2, nl);
-            payload = numbers.substring(nl + 1);
+            int delimiterEndIndex = numbers.indexOf("\n");
+
+            String delimiterPart = numbers.substring(2, delimiterEndIndex);
 
             // single-char custom delimiter
-            if (!header.isEmpty()) {
-                String escaped = java.util.regex.Pattern.quote(header);
-                delimitersRegex = delimitersRegex + "|" + escaped;
+            if (delimiterPart.startsWith("[")) {
+
+                Matcher m = Pattern.compile("\\[(.*?)]").matcher(delimiterPart);
+
+
+                List<String> delimiters = new ArrayList<>();
+
+                while (m.find()) {
+
+                    delimiters.add(Pattern.quote(m.group(1))); // escape regex chars
+
+                }
+                delimitersRegex = String.join("|", delimiters);
+            } else {
+
+                // Single-char delimiter
+
+                delimitersRegex = Pattern.quote(delimiterPart);
+
             }
+            numbers = numbers.substring(delimiterEndIndex + 1);
+
         }
 
-        String[] parts = payload.split(delimitersRegex);
+        String[] tokens = numbers.split(delimitersRegex);
+
+        List<Integer> negatives = new ArrayList<>();
+
         int sum = 0;
-        for (String p : parts) {
-            if (!p.isEmpty()) sum += Integer.parseInt(p);
+
+
+        for (String token : tokens) {
+            if (token.isEmpty()) continue;
+
+            int n = Integer.parseInt(token);
+
+            if (n < 0) negatives.add(n);
+
+            if (n <= 1000) sum += n;
+        }
+
+        if (!negatives.isEmpty()) {
+
+            throw new IllegalArgumentException("negatives not allowed: " + negatives);
+
         }
         return sum;
 
