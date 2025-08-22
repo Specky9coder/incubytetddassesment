a8f14f8bc10b2be21d45001f1efa80bd938c6965
keyur bhadani <bhadani@hectronic.in>
2025-08-22 16:06:53 +0530

    feat:feat: support , and \n delimiters

--- src/main/java/com/incubyte/Calculator.java
+++ src/main/java/com/incubyte/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
     public int add(String numbers) {
 
         if (numbers == null || numbers.isEmpty()) return 0;
-        String[] parts = numbers.split(",");
+        String[] parts = numbers.split(",|\n");
         int sum = 0;
         for (String p : parts) {
             if (!p.isEmpty()) sum += Integer.parseInt(p);
