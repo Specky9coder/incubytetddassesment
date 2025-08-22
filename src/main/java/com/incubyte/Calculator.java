b5366cb9bbe7bdb834a21db650064200303969c9
keyur bhadani <bhadani@hectronic.in>
2025-08-22 15:30:10 +0530

    feat: implement single number

--- src/main/java/com/incubyte/Calculator.java
+++ src/main/java/com/incubyte/Calculator.java
@@ -2,11 +2,12 @@ package com.incubyte;
 
 public class Calculator {
 
+
     public int add(String numbers) {
-        if(numbers.isEmpty()){
-            return 0;
-        }
+
+        if (numbers == null || numbers.isEmpty()) return 0;
         return Integer.parseInt(numbers);
 
     }
+
 }
